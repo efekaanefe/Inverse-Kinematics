931454f223c8eaa83b93bee4e6d908ae0e599a66
efekaanefe <efekaanefe1905@gmail.com>
2024-05-10 18:25:56 +0300

    N link are working

--- 2D-N_Links/main.py
+++ 2D-N_Links/main.py
@@ -24,15 +24,14 @@ if __name__ == "__main__":
             
             # joints
             pygame.draw.circle(screen, JOINT_COLOR, (x1, y1), 5)
-            
             if i == 1:
                 pygame.draw.circle(screen, FIXED_JOINT_COLOR, (x0, y0), 5)
             
 
     # inverse-kinematics
-    N_LINKS = 2
-    LINK_LENGTH = 100/2
-    angles = np.ones(N_LINKS) * -90
+    N_LINKS = 8
+    LINK_LENGTH = 100//3
+    angles = np.ones(N_LINKS) * 15
     link_lengths = np.ones(N_LINKS) * LINK_LENGTH
     
     FIXED_POINT_POSITION = np.array([WIDTH//2, HEIGHT//2])
@@ -41,22 +40,21 @@ if __name__ == "__main__":
         
         def get_distance_difference(angles, link_lengths, target_position): # angles -> x
             positions = get_positions(angles, link_lengths)
-            distance_difference = np.abs(np.sum(target_position - positions[-1]))
-            return distance_difference
+            return np.sqrt((target_position[0] - positions[-1][0])**2 + \
+                    (target_position[1] - positions[-1][1])**2)
 
         optimized_results = minimize(get_distance_difference, angles, method='nelder-mead',
                             args = (link_lengths, target_position) ,options={'xatol': 1e-8, 'disp': True})
-        
-        print(optimized_results)
+
         return optimized_results.x
 
     def get_positions(angles, link_lengths):
         positions = []
         positions.append(FIXED_POINT_POSITION)
-        for i in range(1, N_LINKS+1):
-            prev_pos = positions[i-1]
-            angle = angles[i-1]
-            link_length = link_lengths[i-1]
+        for i in range(N_LINKS):
+            prev_pos = positions[i]
+            angle = angles[i]
+            link_length = link_lengths[i]
             direction = np.array([np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))])
             positions.append(prev_pos + link_length * direction)
         return positions
@@ -75,6 +73,9 @@ if __name__ == "__main__":
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 target_position = event.pos
+            elif pygame.mouse.get_pressed()[0]:
+                target_position = pygame.mouse.get_pos()
+    
         
         if target_position:
             angles = get_optimum_angles(angles, link_lengths, target_position)
